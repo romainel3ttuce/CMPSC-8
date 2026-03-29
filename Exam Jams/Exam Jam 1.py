b415fb138b191de144d50287322678089b8af10e
Romina Sarfehnia <cherrysurf3@gmail.com>
2026-03-29 12:03:25 -0700

    Clean up print statements in Exam Jam 1.py
    Removed unnecessary print statements for clarity.

--- Exam Jams/Exam Jam 1.py
+++ Exam Jams/Exam Jam 1.py
@@ -6,9 +6,7 @@ def digit(n, k):
     return n // 10 ** (k) % 10
 
 print(digit(3579, 2))
-
 print(digit(3579, 0))
-
 print(digit(3579, 10))
 
 # Question 2:
@@ -23,9 +21,7 @@ def clamp(x: float, lo: float, hi: float):
         return x
 
 print(clamp(5.0, 0.0, 10.0))
-
 print(clamp(-2.0, 0.0, 10.0))
-
 print(clamp(99.0, 0.0, 10.0))
 
 # Question 3:
@@ -38,7 +34,5 @@ def max_of_pairs_mins(a: float, b: float, c: float, d: float, e: float, f: float
     return max(min_a_b, min_c_d, min_e_f)
 
 print(max_of_pairs_mins(4.0, 3.7, 6.0, 3.5, 10.0, 9.0))
-
 print(max_of_pairs_mins(1.0, 1.7, 4.5, 3.0, 2.2, 2.2))
-
 print(max_of_pairs_mins(-5.0, -1.0, 0.0, 7.0, 3.0, 9.0))
