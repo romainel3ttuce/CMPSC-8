66e0d3329a5f8d059cdb08f8a02b6182edc9b9f4
Romina Sarfehnia <cherrysurf3@gmail.com>
2026-04-05 23:51:09 -0700

    Update and rename Handout 06.py to Handouts/Handout 06.py

--- Handouts/Handout 06.py
+++ Handouts/Handout 06.py
@@ -1,7 +1,6 @@
 # Handout 06:
 
 # Question 1:
-
 wish = 'Happy Birthday'
 
 print(wish[0].lower() + wish[6].lower())
@@ -10,7 +9,6 @@ print(wish[0].lower() + wish[1:6].lower() + wish[6].lower() + wish[7:])
 print(wish.lower())
 
 # Question 2:
-
 robot = 'R2D2'
 robot2 = 'R2D2$'
 
@@ -21,7 +19,6 @@ print(robot.isdigit())
 print(robot.islower())
 
 # My own test
-
 print(robot2.isupper())
 print(robot2.isalpha())
 print(robot2.isalnum())
@@ -29,7 +26,6 @@ print(robot2.isdigit())
 print(robot2.islower())
 
 # Question 3:
-
 def count_uppercase(s: str) -> int:
     """Return the number of uppercase letters in s.
 
@@ -48,7 +44,6 @@ if __name__ == "__main__":
     doctest.testmod()
 
 # Question 4:
-
 def add_underscores(s: str) -> str:
     """Return a string that contains the characters from s with an underscore
     added after every character except the last.
@@ -70,7 +65,6 @@ if __name__ == "__main__":
     doctest.testmod()
 
 # Question 5:
-
 def every_nth_character(s: str, n: int) -> str:
     """Return a string that contains every nth character from s, starting at
     index 0.
