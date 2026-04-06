c49576deaa7b2d08e61d5f3102cdbdd8d554b804
Romina Sarfehnia <cherrysurf3@gmail.com>
2026-04-05 23:53:47 -0700

    Update and rename Handout 09.py to Handouts/Handout 09 Part 2.py

--- Handouts/Handout 09 Part 2.py
+++ Handouts/Handout 09 Part 2.py
@@ -1,4 +1,4 @@
-# Handout 09:
+# Handout 09 Part 2:
 
 a = 5
 b = 4
@@ -31,7 +31,6 @@ a = 10
 print(id(a))
 
 # Question 1:
-
 a = [0, 1, 2]
 b = a
 b[2] = 100
@@ -45,7 +44,6 @@ print(a == [0, 1, 100] and b == [0, 1, 100])
 print(id(a) == id(b))
 
 # Question 2:
-
 a = [1, 0]
 
 # All of the following code fragments cause a to refer to [1, 0, 8]
@@ -58,7 +56,6 @@ a = a + [8]
 a = [a[0], a[1], 8]
 
 # Question 3:
-
 a = [1, 0, 8]
 b = a.sort()
 
